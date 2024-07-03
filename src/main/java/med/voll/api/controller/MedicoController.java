03bc032faafa7ab56f0145dc6f0436b113407945
FabianoeSouza <fabiano.lotar@yahoo.com.br>
2024-07-03 08:45:46 -0300

    first commit

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -7,13 +7,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
 
 @RestController // o Spring carrega nossa classe
-@RequestMapping("medicos") // mapear a URL /medicos
+@RequestMapping("/medicos") // mapear a URL /medicos
 public class MedicoController {
 
     @Autowired //estou injentanso o repository como sendo um atributo
@@ -21,30 +24,47 @@ public class MedicoController {
 
     @PostMapping // é para voce chamar o metodo cadastrar da classe medicoController
     @Transactional // para ter trasação ativa com o banco de dados.
-    public void cadastrar(@RequestBody  @Valid DadosCadastroMedico dados ) { //@RequestBody é para puxar do corpo da requisição.
-        repository.save(new Medico(dados));
+    public ResponseEntity cadastrar(@RequestBody  @Valid DadosCadastroMedico dados, UriComponentsBuilder uriBuilder) { //@RequestBody é para puxar do corpo da requisição.
+        var medico = new Medico(dados);
+        repository.save(medico);
 
+        var uri = uriBuilder.path("/medicos/{id}").buildAndExpand(medico.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(new DadosDetalhamentoMedico(medico));
     }
 
     @GetMapping
-    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
-        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+    public ResponseEntity <Page<DadosListagemMedico>> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
+        var page =  repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+        return ResponseEntity.ok(page);
     }
 
     @PutMapping
     @Transactional
-    public void atualizar(@RequestBody  @Valid DadosAtualizacaoMedico dados ) {
+    public ResponseEntity  atualizar(@RequestBody  @Valid DadosAtualizacaoMedico dados ) {
         var medico = repository.getReferenceById(dados.id());
         medico.atualizarInformacoes(dados);
+
+        return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
     }
 
     @DeleteMapping("/{id}")
     @Transactional
-    public void excluir(@PathVariable Long id) {
+    public ResponseEntity excluir(@PathVariable Long id) {
         var medico = repository.getReferenceById(id);
         medico.excluir();
+
+        return ResponseEntity.noContent().build();
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity detalhar(@PathVariable Long id) {
+        var medico = repository.getReferenceById(id);
+
+        return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
+    }
 
 }
-//@ Valid serve para o Spring se integrar com o Bean Validation
+//@Valid serve para o Spring se integrar com o Bean Validation
+//subistiuiu void utilizaremos uma classe do Spring chamada ResponseEntity, sendo uma classe que conseguimos controlar
+// a resposta devolvida pelo framework.
